21e11fd8b445643b8965d83da256285f6e65acb7
Fellipe Lima <fellipe_limasilva@hotmail.com>
2023-06-25 15:46:49 -0300

    Adicionado mais três botões e a contagem do total de letras, palavras e linhas.

---
+++ Editor de texto/README.md
@@ -0,0 +1,5 @@
+# ConverterCase
+
+O objetivo desse programa é aplicar os conhecimentos adquiridos durante os estudos.
+
+O intuito desse programa é converter texto em formatos diferente como minúsculo, maiúsculo, primeira letra maiúscula, letras alternadas entre minúsculo e maiúsculo, remover espaços vazios no inicio e no fim do texto, inverte o texto e salvar em formato .txt. 
--- Editor de texto/editor.py
+++ Editor de texto/editor.py
@@ -1,50 +1,101 @@
-# # Texto em caixa alta
-# texto = input("Digite um texto: ")
-# texto_em_caixa_alta = texto.upper()
-# print("Texto em caixa alta:", texto_em_caixa_alta)
-
-# # Texto em caixa baixa
-# texto = input("Digite um texto: ")
-# texto_em_caixa_alta = texto.lower()
-# print("Texto em caixa baixa:", texto_em_caixa_alta)
-
-# # Texto em formato de título
-# texto = input("Digite um texto: ")
-# texto_em_caixa_alta = texto.title()
-# print("Texto em caixa alta:", texto_em_caixa_alta)
-
-# # Converter o texto upcase para lowercase e vise e versa
-# texto = input("Digite um texto: ")
-# texto_em_caixa_alta = texto.swapcase()
-# print("Texto em caixa alta:", texto_em_caixa_alta)
-
 from PyQt5 import uic, QtWidgets
+from PyQt5.QtWidgets import QFileDialog
 
-def caixaAlta():
-    texto = editor.lineEdit.text()
-    texto_em_caixa_alta = texto.upper()
+def primeiraMaiuscula():
+    texto = editor.textEdit.toPlainText()
+    texto_modificado = texto.capitalize()
     
-    editor.lineEdit.clear()
-    
-    editor.lineEdit.setText(texto_em_caixa_alta)
+    editor.textEdit.clear()
+    editor.textEdit.setText(texto_modificado)
 
 def caixaBaixa():
-    texto = editor.lineEdit.text()
-    texto_em_caixa_alta = texto.lower()
+    texto = editor.textEdit.toPlainText()
+    texto_modificado = texto.lower()
     
-    editor.lineEdit.clear()
+    editor.textEdit.clear()
+    editor.textEdit.setText(texto_modificado)
+
+def caixaAlta():
+    texto = editor.textEdit.toPlainText()
+    texto_modificado = texto.upper()
     
-    editor.lineEdit.setText(texto_em_caixa_alta)
+    editor.textEdit.clear()
+    editor.textEdit.setText(texto_modificado)
+
+def textoAlternado():
+    texto = editor.textEdit.toPlainText()
+
+    texto_modificado = ""
+    for i in range(len(texto)):
+        if i % 2 == 0:  # Se o índice for par
+            texto_modificado += texto[i].lower()
+        else:  # Se o índice for ímpar
+            texto_modificado += texto[i].upper()
+    
+    editor.textEdit.clear()
+    editor.textEdit.setText(texto_modificado)
+
+def removerEspaco():
+    texto = editor.textEdit.toPlainText()
+    texto_modificado = texto.strip()
+    
+    editor.textEdit.clear()
+    editor.textEdit.setText(texto_modificado)
+
+def tituloCase():
+    texto = editor.textEdit.toPlainText()
+    texto_modificado = texto.title()
+    
+    editor.textEdit.clear()
+    editor.textEdit.setText(texto_modificado)
+
+def inverseCase():
+    texto = editor.textEdit.toPlainText()
+    texto_modificado = texto[::-1]
+    
+    editor.textEdit.clear()
+    editor.textEdit.setText(texto_modificado)
+
+def baixar():
+    texto = editor.textEdit.toPlainText()
+
+    file_path, _ = QFileDialog.getSaveFileName(None, "Salvar Arquivo", "", "Arquivo de Texto (*.txt)")
+    if file_path:
+        with open(file_path, "w") as file:
+            file.write(texto)
 
 def limpar():
-    editor.lineEdit.clear()
+    editor.textEdit.clear()
+
+def contagem():
+    texto = editor.textEdit.toPlainText()
+
+    letras = len(texto)
+    editor.lbl_letras.setText(str(letras))
+
+    palavras = texto.split()  # Divide o texto em uma lista de palavras
+    contar_palavras = len(palavras)
+    editor.lbl_palavras.setText(str(contar_palavras)) # Retorna o número de palavras na lista
+
+    linhas = texto.splitlines()
+    contar_linhas = len(linhas)
+    editor.lbl_linhas.setText(str(contar_linhas))
 
 
 app = QtWidgets.QApplication([])
 editor = uic.loadUi("editor.ui")
+
+editor.btn_frase.clicked.connect(primeiraMaiuscula)
+editor.btn_caixaBaixa.clicked.connect(caixaBaixa)
 editor.btn_caixaAlta.clicked.connect(caixaAlta)
-editor.btn_caixaBaixa.clicked.connect(caixaAlta)
+editor.btn_alternado.clicked.connect(textoAlternado)
+editor.btn_removerEspaco.clicked.connect(removerEspaco)
+editor.btn_tituloCase.clicked.connect(tituloCase)
+editor.btn_inverseCase.clicked.connect(inverseCase)
+editor.btn_baixar.clicked.connect(baixar)
 editor.btn_limpar.clicked.connect(limpar)
+
+editor.textEdit.textChanged.connect(contagem)
 
 editor.show()
 app.exec()
